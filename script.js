04c8471a89f633ca66ab2e299ca5a109f8ac8ee7
anasouza0087 <anasouza0087@users.noreply.github.com>
2021-01-17 22:09:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,27 @@
-//const tarefa = document.getElementById("tarefa")
 const cadastro = document.getElementById("cadastro")
-const remover = document.getElementById("remover")
 const concluido = document.getElementById("concluido")
 
 cadastro.addEventListener("click", e => {
-    //document.getElementById("item").innerText = 'Item1'
     const novaTarefa = document.createElement("li")
     const tarefaValor = document.getElementById("tarefa").value
+
     novaTarefa.appendChild(document.createTextNode(tarefaValor))
+    novaTarefa.appendChild(document.createElement('button'))
+    novaTarefa.appendChild(document.createElement('button'))
 
     const listaTarefas = document.getElementsByTagName('ol')[0]
     const listaItens = document.getElementsByTagName('li')
-    listaTarefas.insertBefore(novaTarefa, listaItens[0])
+    const listaBtnDel = document.getElementsByTagName('button')
+    const listReady = document.getElementsByTagName('button')
+    listaTarefas.insertBefore(novaTarefa, listaItens[0], listaBtnDel[0], listReady[0])
 
 })
 
-remover.addEventListener("click", e => {
-    document.getElementById("item").innerHTML = ""
-    document.getElementById("item").style.background = "#FFFFFF"
-})
+//remover.addEventListener("click", e => {
 
-concluido.addEventListener("click", e => {
-    document.getElementById("item").style.background = "#FF0000"
-})
+
+//})
+
+//concluido.addEventListener("click", e => {
+
+//})
